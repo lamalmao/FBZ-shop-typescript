76f8a569df94eec7c06c184e3a459c807b8a13ca
lamalmao <laesia>
2023-02-27 15:37:14 +0300

    Некоторые начальные параметры содержатся тут

---
+++ src/properties.ts
@@ -0,0 +1,7 @@
+import path from 'path';
+
+const root = process.cwd();
+
+export const rootDir = path.join(root, 'extras');
+export const imagesDir = path.join(rootDir, 'images');
+export const templatesDir = path.join(rootDir, 'templates');
