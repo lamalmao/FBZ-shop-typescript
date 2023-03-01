06250275760b654b6ec930669871fc1ad5aa72b9
lamalmao <laesia>
2023-03-01 16:36:14 +0300

    Добавлены некоторые параметры

--- src/properties.ts
+++ src/properties.ts
@@ -5,3 +5,5 @@ const root = process.cwd();
 export const rootDir = path.join(root, 'extras');
 export const imagesDir = path.join(rootDir, 'images');
 export const templatesDir = path.join(rootDir, 'templates');
+
+export const GOODS_COVER_TEMPLATE = 'item_cover.pug';
