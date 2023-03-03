81c73a4121f45005363f0c1b26585b713b6f3c41
lamalmao <laesia>
2023-03-03 16:45:42 +0300

    Удалены остатки после отладки

--- src/bot/notice-events/notice.ts
+++ src/bot/notice-events/notice.ts
@@ -1,4 +1,4 @@
-import logger from '../../logger.js';
+// import logger from '../../logger.js';
 import INoticeData from '../../shop/users/notice.js';
 import { ACTIONS } from '../../shop/users/user-constants.js';
 import { User } from '../../shop/users/user.js';
@@ -6,12 +6,10 @@ import EventEmitter from 'events';
 
 const userNoticeEmitter = new EventEmitter();
 
+// userNoticeEmitter.someMiddlewareForLogging
+
 userNoticeEmitter.on(ACTIONS.BALANCE_CHANGED, async (user: User, data?: INoticeData) => {
   console.log(user, data);
-
-  logger.info('Баланс то поменялся ебать, гы');
-  logger.error('test');
-  logger.info('check it out');
 });
 
 export default userNoticeEmitter;
--- src/shop/users/notice.d.ts
+++ src/shop/users/notice.d.ts
@@ -7,4 +7,5 @@ export default interface INoticeData {
   username?: string;
   telegramId?: number;
   oldTelegramId?: number;
+  region?: string;
 }
